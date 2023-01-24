0e8269b06716d4cc02b2ac06ec6c6d697cdcf530
Jac22 <jacopo.prudentino@gmail.com>
2023-01-24 16:30:54 +0100

    Aggiunto metodo per stamapre le info dei film

--- index.php
+++ index.php
@@ -2,16 +2,33 @@
 
 class Movie{
     public $title;
+    public $genre;
     public $year_release;
+    public $publisher;
     public $length;
     public $synopsis;
-    public $vote;
     
-    public function __construct($title, $year, $length, $synopsis, $vote){
+    public function __construct($title, $genre, $year, $publisher, $length, $synopsis){
         $this -> title = $title;
-        $this -> year = $year;
+        $this -> genre = $genre;
+        $this -> year_release = $year;
+        $this -> publisher = $publisher;
         $this -> length = $length;
         $this -> synopsis = $synopsis;
-        $this -> vote = $vote;
-    } 
+    }
+    
+    public function GetMovieInfo(){
+        return $this->title . '<br>' .
+            $this->genre . '<br>' .
+            $this->year_release . '<br>' .
+            $this->publisher . '<br>' .
+            $this->length . '<br>' .
+            $this->synopsis;
+    }
 }
+
+$Avatar = new Movie('Avatar', 'Fantascienza', 2009, '20th Century Fox', '2,39:1', 'lorem ipsum');
+$Luca = new Movie('Luca', 'Animazione', 2021, 'Pixar', '1.85:1', 'lorem ipsum');
+echo $Avatar->GetMovieInfo();
+echo '<br>';
+echo $Luca->GetMovieInfo();
